5a635b0506dbaa3b9e91dde9990668c3dcee4716
xmisao <mail@xmisao.com>
2021-01-01 14:47:38 +0900

    Add install docker

---
+++ install_docker_on_debian.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Set debian version
+lsb=buster
+
+# Install
+sudo apt-get update
+sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/debian \
+  $lsb \
+  stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Add user to docker group
+sudo gpasswd -a $USER docker
+
+# Install docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
