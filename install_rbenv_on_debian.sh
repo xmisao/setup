bb786d8c0b46474e9f917aa1e10cefab3bdd5690
xmisao <mail@xmisao.com>
2021-01-10 13:04:33 +0900

    Add libraries installation

--- install_rbenv_on_debian.sh
+++ install_rbenv_on_debian.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install -y build-essential
+sudo apt-get install -y build-essential ibssl-dev zlib1g-dev
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
